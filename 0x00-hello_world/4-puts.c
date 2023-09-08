c7e341f9c149e4a2080f7275806b37c682f25c64
onaope007 <davidonaopemipo7@gmail.com>
2023-09-07 17:29:55 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- * main-Serves as the entry for the program
- * Return: Always 0(succes)
+/**
+ *  main-Serves as the entry for the program
+ *  Return: By default returns zero if no errors otherwise returns non-zero
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
