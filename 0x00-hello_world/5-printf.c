ffa35935ec90b971bad584a90e4ef432dd9f2c94
onaope007 <davidonaopemipo7@gmail.com>
2023-09-07 17:37:59 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main-Serves as the entry for the program
+ *  Return: By default returns zero if no errors otherwise returns non-zero
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
