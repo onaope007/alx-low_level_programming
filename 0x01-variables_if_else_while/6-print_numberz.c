b6700bb0858fcabd2accec65ff461343b474495b
onaope007 <davidonaopemipo7@gmail.com>
2023-09-09 13:22:38 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *  main -Starting point of the program
+ * Description: main -Prints out a string to stdout
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
