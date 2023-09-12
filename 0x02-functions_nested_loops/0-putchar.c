eda4c492f8bf0ea78a6874613144337e40580ab8
onaope007 <davidonaopemipo7@gmail.com>
2023-09-12 12:26:17 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main -Starting point of the program
  * Description: main -Prints out a string to stdout
  * myFunction: it declares what the main function shoykd carry out
  * Return: 0 if success
  */
-int myFunction(void)
-{
-	printf("_putchar\n");
-}
 int main(void)
 {
-	myFunction();
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
