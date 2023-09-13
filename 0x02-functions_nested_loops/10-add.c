375a36ba604f226e4320fe6b270ebc567f810083
onaope007 <davidonaopemipo7@gmail.com>
2023-09-13 13:07:07 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add - Add two integers
+ *@i: int number
+ *@j: int number
+ *Return: result of addiition
+ */
+int add(int i, int j)
+{
+	int sum = i + j;
+
+	return (sum);
+}
