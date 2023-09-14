9cc653c56028eabadb3da8401152ffd58db3891c
onaope007 <davidonaopemipo7@gmail.com>
2023-09-14 12:27:05 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - The function that carrioes out the task
+ * @a: The first parameter
+ * @b: The second parameter
+ *Return: returns the product of and b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
