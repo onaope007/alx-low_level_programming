ced103d4871bac86c1c38e58db6c8734fb7deeb3
onaope007 <davidonaopemipo7@gmail.com>
2023-09-14 11:53:31 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit - the main function that checks the paameter
+ * @c: the parameter that is to be checked
+ * Return: program returns 1 if true and 0 if false
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
