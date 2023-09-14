20fecece138808c40f7c511bb18a363023d8c58c
onaope007 <davidonaopemipo7@gmail.com>
2023-09-14 11:38:50 -0700

    task 0 of more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper- returns a value of 1 if a variable is upper case
+ * @c: the parameter of the given function
+ * Return: 1 if true and 0 if false
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
